29b92320ca633b5db3df12fc40329a51a523d1d3
Michael Meyer <mcm1792@rit.edu>
2015-04-18 15:48:26 -0400

    Initial commit

---
+++ android/src/brickhack/songbird/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package brickhack.songbird.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import brickhack.songbird.SongBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SongBird(), config);
+	}
+}
