57b748687c647553fe34770aa121919de6b6d168
Sam Chen <ssc7954@rit.edu>
2015-04-19 08:13:13 -0400

    moved to correct package location for SongBird.java

--- core/src/brickhack/songbird/SongBird.java
+++ core/src/brickhack/songbird/SongBird.java
@@ -6,16 +6,14 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
 
-import brickhack.songbird.models.PlayerBlob;
-import brickhack.songbird.util.Notes;
+import brickhack.songbird.models.GameModel;
 
 public class SongBird extends ApplicationAdapter {
 
     private ShapeRenderer shapeRenderer;
+    private SpriteBatch spriteBatch;
+    private GameModel gameModel;
     private int boxWidth = 400;
 
     private PitchInterface pitchInterface;
@@ -31,6 +29,8 @@ public class SongBird extends ApplicationAdapter {
     @Override
 	public void create () {
         this.shapeRenderer = new ShapeRenderer();
+        this.spriteBatch = new SpriteBatch();
+        this.gameModel = new GameModel();
 	}
 
 	@Override
@@ -40,25 +40,12 @@ public class SongBird extends ApplicationAdapter {
 
         System.out.println(this.pitchInterface.getPitch());
 
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 0.4f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-//        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-//        shapeRenderer.setColor(Color.CYAN);
-//
-//        this.boxWidth += Gdx.graphics.getDeltaTime() * 100.0f;
-//
-//        shapeRenderer.rect(0,0, this.boxWidth, this.boxWidth);
-        // added
-        World world = new World(new Vector2(0,0), true);
-        SpriteBatch batch = new SpriteBatch();
-        PlayerBlob blah = new PlayerBlob(world, new Vector2(500,500), 15);
-        batch.begin();
-        blah.draw(batch);
-        batch.end();
+        spriteBatch.begin();
+        gameModel.draw(spriteBatch);
+        spriteBatch.end();
 
-        batch.dispose();
-
-        //shapeRenderer.end();
 	}
 }
