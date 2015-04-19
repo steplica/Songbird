cc1ec4a9bd43306546feb3f5863fd4a660e5c990
Michael Meyer <mcm1792@rit.edu>
2015-04-19 00:55:44 -0400

    JNI stuff

--- android/src/brickhack/songbird/android/AndroidLauncher.java
+++ android/src/brickhack/songbird/android/AndroidLauncher.java
@@ -7,10 +7,15 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import brickhack.songbird.SongBird;
 
 public class AndroidLauncher extends AndroidApplication {
+
+    public native void DoFFT(double[] data, int size);
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new SongBird(), config);
+
+        System.loadLibrary("fft-jni");
 	}
 }
