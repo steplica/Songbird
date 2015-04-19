4fdf9244a8fe1fc5bfb5da518a0c9e49615edc58
Michael Meyer <mcm1792@rit.edu>
2015-04-19 03:09:55 -0400

    Remove JNI

--- android/src/brickhack/songbird/android/AndroidLauncher.java
+++ android/src/brickhack/songbird/android/AndroidLauncher.java
@@ -8,14 +8,10 @@ import brickhack.songbird.SongBird;
 
 public class AndroidLauncher extends AndroidApplication {
 
-    public native void DoFFT(double[] data, int size);
-
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new SongBird(), config);
-
-        System.loadLibrary("fft-jni");
 	}
 }
